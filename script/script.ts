efd366956c31687182cd4723e75d1aec9bf50398
Patrick Daniel <contato.pdani@gmail.com>
2023-09-22 23:13:14 -0300

    fix: date and time formatted to br standard

--- script/script.ts
+++ script/script.ts
@@ -1,6 +1,6 @@
 // elementos DOM
 const h1Element = document.querySelector('#h1')! as HTMLElement
-const dateTime = document.querySelector('#date-time')! as HTMLElement
+const dateElement = document.querySelector('#date-time')! as HTMLElement
 const inputUsd = document.querySelector('#input-usd')! as HTMLInputElement
 const inputBrl = document.querySelector('#input-brl')! as HTMLInputElement
 
@@ -13,9 +13,21 @@ fetch("https://economia.awesomeapi.com.br/last/USD-BRL")
         let usd: number = Number(data.USDBRL.high)
         let formattedUsd: string = usd.toFixed(2)
 
+        // formatação data e hora
+        let date = new Date(data.USDBRL.create_date)
+        let year = date.getFullYear()
+        let month = date.getMonth()
+        let day = date.getDate()
+        let hour = date.getHours()
+        let minutes = date.getMinutes()
+        let seconds = date.getSeconds()
+
+        // Para formatar a data de volta para uma string:
+        const dateString = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')} ${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
+
         // preenchimento do cabeçalho
         h1Element.innerText = `${formattedUsd} Real Brasileiro`
-        dateTime.innerText = data.USDBRL.create_date;
+        dateElement.innerText = dateString
 
         // estado inicial dos inputs
         inputUsd.value = '1'
